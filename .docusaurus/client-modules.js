e4ffe4bec5070d0702d9c9e2cf00c7b5d730e741
Andrew Chan <chanandrew96@yahoo.com.hk>
2021-08-02 14:24:16 +0800

    Add files under docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\user\\Desktop\\Andrew\\Project\\docusaurus\\andrewchansite\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\user\\Desktop\\Andrew\\Project\\docusaurus\\andrewchansite\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\user\\Desktop\\Andrew\\Project\\docusaurus\\andrewchansite\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
+  require("C:\\Users\\user\\Desktop\\Andrew\\Project\\docusaurus\\andrewchansite\\src\\css\\custom.css"),
+];
